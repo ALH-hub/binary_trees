af676d73146bb1fef2007671572cc9cb19485dae
Alhadji Oumate <alhadjioum0@gmail.com>
2024-01-04 17:07:21 +0100

    fixes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,11 +9,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_l = 0, height_r = 0;
 
-	if (tree == NULL)
-		return (0);
+	if (tree != NULL)
+	{
+		height_l = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+		height_r = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+		return (height_l > height_r ? height_l : height_r);
+	}
 
-	height_l = 1 + tree->left ? binary_tree_height(tree->left) : 0;
-	height_r = 1 + tree->right ? binary_tree_height(tree->right) : 0;
-
-	return (height_l > height_r ? height_l : height_r);
+	return (0);
 }
